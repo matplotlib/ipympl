4699b150fcf4bd85d8e9cda3c6b40a0eade83262
Steven Silvester <steven.silvester@ieee.org>
2017-11-20 11:43:25 -0600

    Fix handling of module imports

--- js/src/nb_index.js
+++ js/src/nb_index.js
@@ -8,5 +8,5 @@
 __webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/jupyter-matplotlib/';
 
 // Export widget models and views, and the npm package version number.
-module.exports = require('./mpl_widget.js');
-module.exports['version'] = require('../package.json').version;
+module.exports = require('./index.js');
+
