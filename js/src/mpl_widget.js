f9d983c24d2ecbd94071abd4d3f9604f81903e6c
martinRenou <martin.renou@gmail.com>
2019-06-21 10:04:55 +0200

    Remove websocket proxy

--- js/src/mpl_widget.js
+++ js/src/mpl_widget.js
@@ -19,54 +19,9 @@ var MPLCanvasModel = widgets.DOMWidgetModel.extend({
 
 var MPLCanvasView = widgets.DOMWidgetView.extend({
     render: function() {
-        var that = this;
-        var id = this.model.get('_id');
-        var element = this.el;
-        element.classList.add('jupyter-widgets');
-
-        this.ws_proxy = this.comm_websocket_adapter(this.model.comm);
-
-        function ondownload(figure, format) {
-           var save = document.createElement('a');
-           save.href = figure.image.src;
-           save.download = figure.header.textContent + '.png';
-           document.body.appendChild(save);
-           save.click();
-           document.body.removeChild(save);
-        }
-
-        mpl.toolbar_items = this.model.get('_toolbar_items')
-
-        var fig = new mpl.figure(id, this.ws_proxy,
-                                 ondownload,
-                                 element);
-
-        // Call onopen now - mpl needs it, as it is assuming we've passed it a real
-        // web socket which is closed, not our websocket->open comm proxy.
-        this.ws_proxy.onopen();
-
-        fig.parent_element = element;
-
-        // subscribe to incoming messages from the MPLCanvasWidget
-        this.model.on('msg:custom', this.ws_proxy.onmessage, this);
+        var fig = new mpl.figure(this.model.get('_id'), this, this.model.get('_toolbar_items'));
 
         this.send(JSON.stringify({ type: 'initialized' }));
-    },
-
-    comm_websocket_adapter: function(comm) {
-        // Create a "websocket"-like object which calls the given comm
-        // object with the appropriate methods. Currently this is a non binary
-        // socket, so there is still some room for performance tuning.
-        var ws = {};
-        var that = this;
-
-        ws.close = function() {
-            comm.close()
-        };
-        ws.send = function(m) {
-            that.send(m);
-        };
-        return ws;
     }
 });
 
