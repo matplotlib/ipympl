028a10aa97d0aa149ddf6b4bca17cc4dcd85843e
martinRenou <martin.renou@gmail.com>
2021-01-05 14:42:34 +0100

    Fix publicPath
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -42,6 +42,7 @@ module.exports = [
             filename: 'index.js',
             path: nbextensionDir,
             libraryTarget: 'amd',
+            publicPath: '',
         },
         devtool: 'source-map',
         module: {
