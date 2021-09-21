4ece515c09488f372290c1b7a84a380857356e78
martinRenou <martin.renou@gmail.com>
2021-09-21 18:22:03 +0200

    Fix prettier

--- src/plugin.ts
+++ src/plugin.ts
@@ -11,12 +11,12 @@ const EXTENSION_ID = 'matplotlib-jupyter:main';
 /**
  * The plugin.
  */
-const jupyterMatplotlibPlugin: IPlugin<Application<Widget>, void> = ({
+const jupyterMatplotlibPlugin: IPlugin<Application<Widget>, void> = {
     id: EXTENSION_ID,
     requires: [IJupyterWidgetRegistry],
     activate: activateWidgetExtension,
     autoStart: true,
-} as unknown) as IPlugin<Application<Widget>, void>;
+} as unknown as IPlugin<Application<Widget>, void>;
 // the "as unknown as ..." typecast above is solely to support JupyterLab 1
 // and 2 in the same codebase and should be removed when we migrate to Lumino.
 
